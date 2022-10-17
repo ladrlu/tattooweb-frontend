c3062ac3187f9e5ed6fb4caa6108696f5438b2b1
Lucie Ládrová <l.ladrova@gmail.com>
2022-10-17 20:14:19 +0200

    Fix image query string, add priority and quality prop

--- components/ImageExtended.tsx
+++ components/ImageExtended.tsx
@@ -10,13 +10,15 @@ const strapi_url = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 const ImageExtended = ({
   src,
-  alt,
   fallBackSrc,
+  alt,
   width,
   height,
+  layout = "intrinsic",
   objectFit = "contain",
   objectPosition = "center center",
   priority = false,
+  quality = 75,
   className,
 }: ImageExtendedProps) => {
   const [imageError, setImageError] = useState(false);
@@ -25,16 +27,18 @@ const ImageExtended = ({
       <Image
         src={
           imageError
-            ? `${strapi_url}${fallBackSrc}?format=png;width=${width};height=${height}`
-            : `${strapi_url}${src}?format=webp;width=${width};height=${height}`
+            ? `${strapi_url}${fallBackSrc}?format=png&width=${width}&height=${height}&quality=${quality}`
+            : `${strapi_url}${src}?format=webp&width=${width}&height=${height}&quality=${quality}`
         }
         alt={alt}
         width={width}
         height={height}
+        layout={layout}
         objectFit={objectFit}
         objectPosition={objectPosition}
-        onError={() => setImageError(true)}
         priority={priority}
+        quality={quality}
+        onError={() => setImageError(true)}
       ></Image>
     </div>
   );
